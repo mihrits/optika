b59f9cc234ea36adf987588ad873c13bdfc57c08
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2020-05-01 02:08:26 +0300

    add script for making difraction pattern plots

--- 2020kevad/nadal12/difraktsioonijoonis.jl
+++ 2020kevad/nadal12/difraktsioonijoonis.jl
@@ -9,11 +9,11 @@ I(I0,b,d,λ,N,sϕ) = I0 * (sin(u(b,λ,sϕ)) / u(b,λ,sϕ))^2 * (sin(N*u(d,λ,sϕ
 
 I_mod(I0,b,d,λ,N,sϕ) = N^2 * I0 * (sin(u(b,λ,sϕ)) / u(b,λ,sϕ))^2
 
-sϕ = -0.005:0.00001:0.005
+sϕ = -0.005:0.000001:0.005
 I0 = 1
 b = 200e-6
 d = 600e-6
-λ = 500e-9
+λ = 550e-9
 N = 8
 
 I1 = I.(I0, b, d, λ, N, sϕ)
@@ -22,3 +22,4 @@ I1_mod = I_mod.(I0, b, d, λ, N, sϕ)
 
 plot(sϕ, I1, leg=false)
 plot!(sϕ, I1_mod, leg=false, s=:dash)
+savefig("difraction_N8_b200_d600.pdf")
